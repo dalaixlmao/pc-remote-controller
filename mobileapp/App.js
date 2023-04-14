7f6c472783d71bb1d50fb1f5132596217c51316e
SuryanshBisht <suryansh4bisht@gmail.com>
2023-04-14 16:54:49 +0530

    login page added

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, PixelRatio } from 'react-native';
 import Devices from './screens/devices.js';
+import Remote from './screens/Remote.js';
+import LoginPage from './screens/loginPage.js'
 import {LinearGradient} from 'expo-linear-gradient';
 
 
@@ -12,8 +14,9 @@ export default function App() {
       colors = {["#9887FF", "#6892FF" , "#68B7FF"]}
       locations = {[0 ,.5, 1]}
       >
-      <Devices/>
-      <Remote style={styles.remote}/>
+      {/*<Devices/>*/}
+      <LoginPage/>
+      {/*<Remote style={styles.remote}/>*/}
       <StatusBar style="auto" />
     </LinearGradient>
     </View>
@@ -27,5 +30,10 @@ const styles = StyleSheet.create({
     // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  remote: {
+    flex: 1,
+    justifyContent:'center',
+    alignItems:'center',
   },
 });
